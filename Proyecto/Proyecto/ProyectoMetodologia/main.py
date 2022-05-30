0e48690ecf701da59125e272218ad9b49a4f4cf6
MatiasEzequielRoselot <104525369+MatiasEzequielRoselot@users.noreply.github.com>
2022-05-30 14:57:54 -0300

    Add files via upload

---
+++ Proyecto/Proyecto/ProyectoMetodologia/main.py
@@ -0,0 +1,39 @@
+from typing_extensions import Self
+import pygame
+
+
+pygame.init()
+
+
+clock = pygame.time.Clock()
+fps=60
+
+
+LargoPantalla= 1353
+AnchoPantalla=600
+
+screen = pygame.display.set_mode((LargoPantalla,AnchoPantalla))
+
+
+fondo = pygame.image.load('imagenes/tableroPrueba.png').convert_alpha()
+
+class personaje():
+    def __init__(self):
+        pass
+
+def dibFondo():
+    screen.blit(fondo,(0,0))
+
+run = True
+
+while run:
+    clock.tick(fps)
+
+    dibFondo()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run=False
+    pygame.display.update()
+
+pygame.quit
