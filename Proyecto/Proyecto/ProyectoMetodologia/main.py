69a45f8be55b657564eb5da3ffc7bbf59a149d78
MatiasEzequielRoselot <104525369+MatiasEzequielRoselot@users.noreply.github.com>
2022-06-02 16:53:59 -0300

    rango

--- Proyecto/Proyecto/ProyectoMetodologia/main.py
+++ Proyecto/Proyecto/ProyectoMetodologia/main.py
@@ -12,6 +12,7 @@ pygame.init()
 clock = pygame.time.Clock()
 fps=60
 verde=(0,255,0)
+rojo=(255,0,0)
 negro=(0,0,0)
 
 LargoPantalla= 1353
@@ -31,9 +32,10 @@ class cartaOfen():
         self.rangoMin=rangoMin
 
 class carta():
-    def __init__(self,nombre,tipoCarta):
+    def __init__(self,nombre,tipoCarta,nomTipo):
         self.nombre=nombre
         self.carta=tipoCarta
+        self.nomTipo=nomTipo
         self.foto=pygame.image.load(f'imagenes/{self.nombre}.png')
 
     def dibujar(self,x,y):
@@ -50,9 +52,51 @@ class personaje():
         self.carta= pygame.image.load(f'imagenes/{self.nombre}.png')
         self.mazo=mazo
 
-    def turno(self):
-        self.jugo=False
-        pass
+    def turno(self,run,posciones,i):
+        #es la carta que se hizo clik
+        jugada:carta=self.mazo[i]
+        lineaObj=0
+        posself=0
+        #revisa si la carta es ofenciva/defensiva y elije en que linea se juega
+        for x in posiciones:
+            if x.pers==self :
+                posself=x.pos
+                if x.lado==1:
+                    if jugada.nomTipo=="of":
+                        lineaObj=2
+                    else:
+                        lineaObj=1
+                elif x.lado==2:
+                    if jugada.nomTipo=="of":
+                        lineaObj=1
+                    else:
+                        lineaObj=2 
+
+        clik=False
+        while run==True :
+            #pone en rojo los lugares que llega la carta ofenciva
+            if jugada.nomTipo=="of":
+                for x in posiciones:
+                    if x.lado==lineaObj and ((posself+jugada.carta.rangoMax)>=x.pos>=(posself+jugada.carta.rangoMax)):
+                        x.dibujar(rojo)
+            
+
+            #sige revisando los eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run=False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    clik=True
+                    posmouse=pygame.mouse.get_pos()
+                else:
+                    clik=False    
+                    posmouse=(0,0)
+                    
+            pygame.display.update()        
+               
+
+    pygame.display.update()
+
 
     def toca(self):
         pass
@@ -71,11 +115,13 @@ class posicion():
         self.lado=lado
         self.pers=pers
 
-    def dibujar(self):
-        pygame.draw.circle(screen,verde,(self.x,self.y),5.0)
+    def dibujar(self,color=verde):
+        pygame.draw.circle(screen,color,(self.x,self.y),5.0)
         if self.pers.nombre != "null":
             self.pers.dibujar(self)
-    
+
+       
+#esta roto    
 class BotonMov():
     def __init__(self,x,y):
         self.x=x
@@ -86,10 +132,10 @@ class BotonMov():
     def mover(per:personaje ,pos)  :
         pass
     
-    
+#personajes generales y la posicion en el array de posiciones    
 null=personaje(0,0,"null",0,0,0,[])
-per1=personaje(0,0,"Espadachin",10,2,4,[carta('Espadachin',cartaOfen(3,1,0)),carta('Espadachin',cartaOfen(3,1,0))])
-per2=personaje(0,0,"Espadachin",10,2,3,[carta('Espadachin',cartaOfen(3,1,0))])
+per1=personaje(0,0,"Espadachin",10,2,4,[carta('Espadachin',cartaOfen(3,1,0),"of"),carta('Espadachin',cartaOfen(3,1,0),"of")])
+per2=personaje(0,0,"Espadachin",10,2,3,[carta('Espadachin',cartaOfen(3,1,0),"of")])
 #per2=personaje(0,0, "Desesperada",6,1,3)
 #per3=personaje(0,0,"Enfermo",10,1,3)
 #per4=personaje(0,0"Bandida",12,2,2)
@@ -112,7 +158,7 @@ pos4B=posicion(1170,380,4,2,null)
 
 posiciones=[pos1A,pos2A,pos3A,pos4A,pos1B,pos2B,pos3B,pos4B]
 turnos=[]
-
+# crea el orden de turnos
 def agregarturnos(turnos,posiciones):
     for i in range(8):
         per=personaje(0,0,"null",0,0,0,[])
@@ -122,56 +168,56 @@ def agregarturnos(turnos,posiciones):
                     per=j.pers
         per.jugo=True                
         turnos.append(per)                
-
+#funcion que dibuja el fondo
 def dibFondo():
     screen.blit(fondo,(0,0))
     pygame.draw.rect(screen,negro,(0,AnchoPantalla-Partemano/2,LargoPantalla,150))
-    
+
+#datos generales    
 boton=BotonMov(1203,AnchoPantalla-Partemano/2)
 run = True
 clik=False
 espacio=0
 posmouse=[]
 toca=0
-
+tamañoCartas=150
 agregarturnos(turnos,posiciones)
 
 
 while run:
     clock.tick(fps)
-
+    #dibuja todo
     dibFondo()
     boton.dibujar()
-    
     for i in posiciones:
         i.dibujar()
     
     espacio=0
     for i in turnos[toca].mazo:
         i.dibujar(espacio,AnchoPantalla-Partemano/2)
-        espacio += 150
-
+        espacio += tamañoCartas
+    #si hubo un clik revisa si fue en una carta
     if clik==True and AnchoPantalla-Partemano/2<=posmouse[1]<=AnchoPantalla+Partemano:
         espacio=0
-        mano:carta=[]
         mano=turnos[toca].mazo
         i=0
         while i<mano.__len__():
-            if espacio<=posmouse[0] and posmouse[0]<=espacio+150:
-                turnos[toca].turno()
+            if espacio<=posmouse[0]<=espacio+150:
+                turnos[toca].turno(run,posiciones,i)
                 if toca<=8:
                     toca +=1
                 else: 
                     toca=0  
                 break               
             else:
-                espacio +=150 
-            i +=1       
+                espacio +=tamañoCartas
+                i +=1
+                   
     
         
 
     
-    
+    #esto revisa los eventos(clik y salir)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run=False
