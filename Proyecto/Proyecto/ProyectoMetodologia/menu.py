9180b624ec5137a7121bff4e78f97b64185e569a
MatiasEzequielRoselot <104525369+MatiasEzequielRoselot@users.noreply.github.com>
2022-06-29 13:13:13 -0300

    version 0.3
    Menu para iniciar y salir del juego

--- Proyecto/Proyecto/ProyectoMetodologia/menu.py
+++ Proyecto/Proyecto/ProyectoMetodologia/menu.py
@@ -1,5 +1,6 @@
 import pygame
 import objeto
+import partida
 
 white=(255, 255, 255)
 black=(0, 0, 0)
@@ -11,6 +12,8 @@ yellow=(255, 255, 0)
 font = None
 clock = pygame.time.Clock()
 fps=60
+posmouse=[]
+clik=False
 
 
 def text_format(message, textFont, textSize, textColor):
@@ -22,7 +25,6 @@ def text_format(message, textFont, textSize, textColor):
 def main_menu(screen,largoPantalla):
     screen_width=largoPantalla
     menu=True
-    selected="start"
     posmouse=[]
     while menu:
         posmouse=pygame.mouse.get_pos()
@@ -34,25 +36,23 @@ def main_menu(screen,largoPantalla):
  
         # Main Menu UI
         screen.fill(blue)
-        title=text_format("Sourcecodester", font, 90, yellow)
+        title=text_format("Menu Principal", font, 90, yellow)
         text_start = text_format("Inicio", font, 75, black)
-        text_mazos = text_format("Mazos",font,75,black)
         text_quit = text_format("Salir", font, 75, black)
         title_rect=title.get_rect()
         start_rect=text_start.get_rect()
-        mazos_rect=text_mazos.get_rect()
         quit_rect=text_quit.get_rect()
 
         if screen_width/2 - (start_rect[2]/2)<=posmouse[0]<=screen_width/2+(start_rect[2]/2) and 300<=posmouse[1]<=start_rect[3]+300:
             text_start=text_format("Inicio", font, 75, white)
+            if clik==True:
+                partida.jugar(objeto.tablero)
         else:
             text_start = text_format("Inicio", font, 75, black)
-        if screen_width/2 - (start_rect[2]/2)<=posmouse[0]<=screen_width/2+(start_rect[2]/2) and 360<=posmouse[1]<=start_rect[3]+360:
-            text_mazos = text_format("Mazos",font,75,white)
-        else:
-            text_mazos = text_format("Mazos",font,75,black)
         if screen_width/2 - (quit_rect[2]/2)<=posmouse[0]<=screen_width/2+(quit_rect[2]/2) and 420<=posmouse[1]<=quit_rect[3]+420:
             text_quit=text_format("Salir", font, 75, white)
+            if clik==True:
+                pygame.quit()
         else:
             text_quit = text_format("Salir", font, 75, black)
  
@@ -63,11 +63,66 @@ def main_menu(screen,largoPantalla):
         # Main Menu Text
         screen.blit(title, (screen_width/2 - (title_rect[2]/2), 80))
         screen.blit(text_start, (screen_width/2 - (start_rect[2]/2), 300))
-        screen.blit(text_mazos,(screen_width/2 - (mazos_rect[2]/2),360))
         screen.blit(text_quit, (screen_width/2 - (quit_rect[2]/2), 420))
 
         
 
+
+        pygame.display.update()
+
+
+
+def victoria(screen,largoPantalla,ganador):
+
+    screen_width=largoPantalla
+    menu=True
+    posmouse=[]
+    while menu:
+        posmouse=pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                clik=True
+                
+                
+
+ 
+        # Main Menu UI
+        screen.fill(blue)
+        title=text_format("El Ganador es:", font, 90, yellow)
+        gana=text_format(ganador,font,75,white)
+        text_start = text_format("Inicio", font, 75, black)
+        text_mazos = text_format("Mazos",font,75,black)
+        text_quit = text_format("Salir", font, 75, black)
+        title_rect=title.get_rect()
+        gana_rect=gana.get_rect()
+        start_rect=text_start.get_rect()
+        mazos_rect=text_mazos.get_rect()
+        quit_rect=text_quit.get_rect()
+
+        if screen_width/2 - (start_rect[2]/2)<=posmouse[0]<=screen_width/2+(start_rect[2]/2) and 300<=posmouse[1]<=start_rect[3]+300:
+            text_start=text_format("Revancha", font, 75, white)
+            if clik==True:
+                partida.jugar(objeto.tablero)
+        else:
+            text_start = text_format("Revancha", font, 75, black)
+        if screen_width/2 - (quit_rect[2]/2)<=posmouse[0]<=screen_width/2+(quit_rect[2]/2) and 420<=posmouse[1]<=quit_rect[3]+420:
+            text_quit=text_format("Salir", font, 75, white)
+            if clik==True:
+                pygame.quit()
+        else:
+            text_quit = text_format("Salir", font, 75, black)
+ 
+        title_rect=title.get_rect()
+        start_rect=text_start.get_rect()
+        quit_rect=text_quit.get_rect()
+ 
+        # Main Menu Text
+        screen.blit(title, (screen_width/2 - (title_rect[2]/2), 80))
+        screen.blit(text_start, (screen_width/2 - (start_rect[2]/2), 300))
+        screen.blit(text_quit, (screen_width/2 - (quit_rect[2]/2), 420))
 
         pygame.display.update()
 
